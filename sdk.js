6f67afd941909fd4cee169ec0b7dc1b730036bd6
ReversedK <reversed.k@gmail.com>
2021-08-16 11:42:49 +0200

    added logos

--- sdk.js
+++ sdk.js
@@ -31,9 +31,21 @@ function solomo_parse(text){
     }
   }
 
-function call_solomo(data){
+
+function call_solomo(data,options={theme:"dark",bgcolor:"green",color:"#fff"}){
     if(data.bien_id===false) return; // no corresponding URL
-   let button_code = "<div style='position:absolute;width: 158px;height:100px;bottom:75px;right:35px;'><a target='_blank' href='"+wwwroot+"/rdvs/rdv/"+data.bien_id+"' style='text-decoration:none;background-color: red;color:#fff;padding: 10px;text-align: center;border-radius: 10px;'>Prendre RDV</a></div>";
+    let img_src;
+    switch(theme){
+      case "dark":
+        img_src= "logo-solomoW.svg" ;
+      break;
+      case "light":
+        img_src="logo-solomoB.svg";
+        break;
+    }
+    const CDN_URL = "https://cdn.jsdelivr.net/gh/agenda-immo/minisdk@main/"
+    img_src = CDN_URL + img_src
+   let button_code = "<div style='position:absolute;width: 158px;height:100px;bottom:75px;right:35px;'><a target='_blank' href='"+wwwroot+"/rdvs/rdv/"+data.bien_id+"' style='text-decoration:none;background-color: "+bgcolor+";color:"+color+";padding: 10px;text-align: center;border-radius: 10px;'><img src='"+img_src+"' style='width: 19%; height: auto;'> Prendre RDV</a></div>";
    let div = document.createElement("div");  
     div.innerHTML = button_code;
    document.body.appendChild(div);
