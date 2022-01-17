bc3dffcaf39ea320c6fd319d011db399b8d2eaa9
reversedk <reversed.k@gmail.com>
2022-01-17 16:06:19 +0100

    (added) bouton prise rdv negociateur

--- sdk.js
+++ sdk.js
@@ -1,7 +1,7 @@
 /**************
  * SDK SolomoBooking
- * version : 0.8
- * date : 12/01/2022
+ * version : 0.9
+ * date : 17/01/2022
  */
 
 function agendaimmo_xhr(type, url, data, options) {
@@ -34,7 +34,7 @@ function agendaimmo_parse(text) {
   }
 }
 
-function call_agendaimmo(data, options) {
+function agendaimmo_generate_button(data, options) {
   if (data.bien_id === false) return; // no corresponding URL
   let img_src;
   switch (options.theme_logo) {
@@ -90,9 +90,69 @@ function agendaimmo(agence_id, ref_interne, user_options = false) {
   window.addEventListener("load", () => {
     let endpoint = wwwroot + "/biens/check/" + agence_id;    
     agendaimmo_xhr("POST", endpoint, "u=" + ref_interne, {
-      success: (r) => call_agendaimmo(r, options),
+      success: (r) => agendaimmo_generate_button(r, options),
     });
   });
+}
+
+function agendaimmo_negociateur(agence_id, negoEmailOrPhone, user_options = false) {
+  let options;
+  if(user_options ===  false) options = default_options;
+  else {
+    options = default_options;
+    Object.keys(user_options).forEach(key => {
+      options[key] = user_options[key];      
+    });
+  }
+
+  wwwroot = "https://pro.agendaimmo.com";
+
+  window.addEventListener("load", () => {
+    let endpoint = wwwroot + "/agents/check/" + agence_id +"/"+encodeURI(negoEmailOrPhone);    
+    agendaimmo_xhr("GET", endpoint,null,  {
+      success: (r) => agendaimmo_generate_button_negociateur(r, options.negociateur),
+    });
+  });
+}
+
+function agendaimmo_generate_button_negociateur(data, options) {
+  if (data.url === false) return; // no corresponding URL
+  let img_src;
+  switch (options.theme_logo) {
+    case "dark":
+      img_src = "logo-solomoB.png";
+      break;
+    case "light":
+      img_src = "logo-solomoW.png";
+      break;
+  }
+  const CDN_URL = "https://cdn.jsdelivr.net/gh/agenda-immo/minisdk@main/";
+  img_src = CDN_URL + img_src;
+  let button_code =
+    "<div style='" +
+    options.styles_container +
+    "'><a target='_blank' href='" +
+    data.url +    
+    "' style='background-color: " +
+    options.bgcolor +
+    ";color:" +
+    options.color +
+    ";" +
+    options.styles_button +
+    "'>" +
+    "<img src='" +
+    img_src +
+    "' style='" +
+    options.styles_icon +
+    "'> " +
+    options.text +
+    "</a></div>";
+  let div = document.createElement("div");
+  div.innerHTML = button_code;
+  if(options.id){
+    const target = document.getElementById(options.id)
+    target.appendChild(div)
+  } else document.body.appendChild(div);
 }
 
 const default_options = {
@@ -105,4 +165,17 @@ const default_options = {
   theme_logo: "light",
   bgcolor: "#ff2127",
   color: "#fff",
+
+  negociateur : {
+    id : '', //div id
+    styles_container:
+    "position:absolute;width: 380px;height:100px;top:75px;right:0px;z-index:999",
+styles_button:
+    "text-decoration:none;text-align: center;display: flex;align-items: center;justify-content: center;font-size: 16px;font-family: Arial;",
+styles_icon: "width: 10%; height: auto;margin-right:10px",  
+text: "Prendre rendez-vous",
+theme_logo: "light",
+bgcolor: "#ffffff",
+color: "#000000"
+  }
 };
